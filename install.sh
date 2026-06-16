dfec3a216bc9e8ae91accf85a55bd574d0205119
Trist4n <tristan@defachel.fr>
2026-06-16 10:56:42 +0200

    feat: add Troubleshooting script

--- install.sh
+++ install.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+echo "                         _                 _   __      _               ";
+echo "  /\\  /\\_   _ _ __  _ __| | __ _ _ __   __| | / _\\ ___| |_ _   _ _ __  ";
+echo " / /_/ / | | | '_ \\| '__| |/ _\` | '_ \\ / _\` | \\ \\ / _ \\ __| | | | '_ \\ ";
+echo "/ __  /| |_| | |_) | |  | | (_| | | | | (_| | _\\ \\  __/ |_| |_| | |_) |";
+echo "\\/ /_/  \\__, | .__/|_|  |_|\\__,_|_| |_|\\__,_| \\__/\\___|\\__|\\__,_| .__/ ";
+echo "        |___/|_|                                                |_|    ";
+echo ""
+echo "By Tris4n"
+
 source "./utils.sh"
 
 source "./scripts-setup/setup-dependencies.sh"
@@ -6,3 +15,5 @@ source "./scripts-setup/setup-dependencies.sh"
 if ask_yes_no "Do you want to configure the theme system (Pywal) ?"; then
     source "./scripts-setup/setup-pywal-theme.sh"
 fi
+
+echo -e "🤔 Having trouble? Use the troubleshooting script to resolve the issue. Run the following command: ${BOLD}sh troubleshooting.sh${NC}"
---
+++ troubleshooting.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# =============================================================
+#  Troubleshooting
+# =============================================================
+set -e
+source "./utils.sh"
+
+options=("C Cedilla does not work" "Quit")
+xcompose="$HOME/.XCompose"
+
+echo -e "\n🤔 What do you want to fix?"
+
+select opt in "${options[@]}"; do
+    case $opt in
+        "C Cedilla does not work")
+             if [ ! -f "$xcompose" ]; then
+                log_info "File $xcompose does not exist. Creating it..."
+                touch "$xcompose"
+            fi
+
+            if ! grep -q "ç" "$xcompose"; then
+                echo '<dead_acute> <c>     : "ç"' >> $xcompose
+                echo '<dead_acute> <C>     : "Ç"' >> $xcompose
+                log_success "Done: Cedilla fix applied."
+            else
+                log_info "Cedialla's fix is ​​already done"
+            fi
+            break
+            ;;
+        "Quit")
+            echo "Exiting."
+            exit 0
+            ;;
+        *) 
+            echo "Invalid option $REPLY" 
+            ;;
+    esac
+done
