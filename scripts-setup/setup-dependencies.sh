aefee4d6cd023259a9f1ed64fabce79856c86e37
Trist4n <tristan@defachel.fr>
2026-06-19 19:28:07 +0200

    feat: Add Oh My Zsh install

--- scripts-setup/setup-dependencies.sh
+++ scripts-setup/setup-dependencies.sh
@@ -8,7 +8,7 @@ source "./utils.sh"
 # Pacman packages
 log_step "Installing core and basic packages via Pacman..."
 sudo pacman -S --needed --noconfirm \
-    nvim yay fish brightnessctl pavucontrol blueman \
+    zsh nvim yay brightnessctl pavucontrol blueman \
     ttf-jetbrains-mono-nerd \
     wl-clipboard hyprlock hypridle \
     qt5ct qt6ct cliphist sed rofi flameshot yazi
@@ -16,10 +16,19 @@ sudo pacman -S --needed --noconfirm \
 # Yay packages
 log_step "Installing AUR packages..."
 yay -S --needed --noconfirm \
-    waybar wlogout swaync \
+    waybar-git wlogout swaync nm-connection-editor \
     dolphin \
     vscodium-bin zen-browser pear-desktop logiops \
     awww python-pywal16 wpgtk nwg-look papirus-icon-theme kvantum \
+
+log_step "Install Oh My Zsh..."
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    log_success "Oh My Zsh has been installed"
+else
+    log_info "Oh My Zsh is already installed."
+fi
 
 echo ""
 echo "🎉 Dependencies Setup complete!"
