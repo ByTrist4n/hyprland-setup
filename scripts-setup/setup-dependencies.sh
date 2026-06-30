2e272288052278e8672e4b09f275bc23e360425d
Trist4n <tristan@defachel.fr>
2026-06-30 22:26:51 +0200

    refactor(waybar): install "waybar-git" and install "libcava"

--- scripts-setup/setup-dependencies.sh
+++ scripts-setup/setup-dependencies.sh
@@ -16,10 +16,13 @@ sudo pacman -S --needed --noconfirm \
 # Yay packages
 log_step "Installing AUR packages..."
 yay -S --needed --noconfirm \
-    waybar-cava wlogout swaync nm-connection-editor \
+    wlogout swaync nm-connection-editor \
     dolphin \
     vscodium-bin zen-browser pear-desktop logiops \
     awww python-pywal16 wpgtk nwg-look papirus-icon-theme kvantum \
+    libcava
+
+_with_cava=true yay -S --needed --noconfirm waybar-git 
 
 log_step "Install Oh My Zsh..."
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
