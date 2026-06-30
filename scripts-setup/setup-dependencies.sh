381b0654a4aa3f495600c682214d49ebf377a973
Trist4n <tristan@defachel.fr>
2026-06-30 16:52:33 +0200

    fix(waybar): "Cava" module not work with AUR "waybar-git"

--- scripts-setup/setup-dependencies.sh
+++ scripts-setup/setup-dependencies.sh
@@ -16,7 +16,7 @@ sudo pacman -S --needed --noconfirm \
 # Yay packages
 log_step "Installing AUR packages..."
 yay -S --needed --noconfirm \
-    waybar-git wlogout swaync nm-connection-editor \
+    waybar-cava wlogout swaync nm-connection-editor \
     dolphin \
     vscodium-bin zen-browser pear-desktop logiops \
     awww python-pywal16 wpgtk nwg-look papirus-icon-theme kvantum \
