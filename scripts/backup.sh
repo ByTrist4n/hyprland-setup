4c7b5f327ec91b580f49092d42ad221230c401db
Trist4n <tristan@defachel.fr>
2026-09-02 17:31:08 +0200

    feat(backup dot files): added spinner backup step

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -9,14 +9,19 @@ log_step "Creating a full backup of existing ~/.config and shell files..."
 
 TIMESTAMP=$(date +%Y%m%d_%H%M%S)
 
-# Backup the whole ~/.config directory if it exists and is not empty
-if [ -d "$HOME/.config" ] && [ "$(ls -A "$HOME/.config")" ]; then
-    cp -rf "$HOME/.config" "$HOME/.config.bak_$TIMESTAMP"
-    log_success "Entire ~/.config backed up to ~/.config.bak_$TIMESTAMP"
-fi
+(
+    # Backup the whole  ~/.config directory if it exists and is not empty
+    if [ -d "$HOME/.config" ] && [ "$(ls -A "$HOME/.config")" ]; then
+        cp -rf "$HOME/.config" "$HOME/.config.bak_$TIMESTAMP"
+        log_success "Entire ~/.config backed up to ~/.config.bak_$TIMESTAMP"
+    fi
 
-# Backup existing .zshrc if it exists and is not a symlink
-if [ -f "$HOME/.zshrc" ] && [ ! -L "$HOME/.zshrc" ]; then
-    mv "$HOME/.zshrc" "$HOME/.zshrc.bak_$TIMESTAMP"
-    log_info "Existing .zshrc backed up to .zshrc.bak_$TIMESTAMP"
-fi
+    # Backup existing .zshrc if it exists and is not a symlink
+    if [ -f "$HOME/.zshrc" ] && [ ! -L "$HOME/.zshrc" ]; then
+        mv "$HOME/.zshrc" "$HOME/.zshrc.bak_$TIMESTAMP"
+        log_info "Existing .zshrc backed up to .zshrc.bak_$TIMESTAMP"
+    fi
+) >/dev/null 2>&1 &
+
+spin $!
+log_success "Your current configuration have been saved!"
