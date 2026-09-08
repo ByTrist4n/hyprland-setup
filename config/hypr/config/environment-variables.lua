193d9895bcf6292a1ba21556b5e9d9e4cf1e107a
Trist4n <tristan@defachel.fr>
2026-09-08 18:23:37 +0200

    feat(hypr): separate Hyprland file

---
+++ config/hypr/config/environment-variables.lua
@@ -0,0 +1,13 @@
+-------------------------------
+---- ENVIRONMENT VARIABLES ----
+-------------------------------
+
+-- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
+
+hl.env("XCURSOR_SIZE", "24")
+hl.env("HYPRCURSOR_SIZE", "24")
+hl.env("LIBVA_DRIVER_NAME", "nvidia")
+hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
+hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
+hl.env("XDG_MENU_PREFIX", "arch-")
+hl.env("QT_IM_MODULE", "wayland;fcitx;ibus")
