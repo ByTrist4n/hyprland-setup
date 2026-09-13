312406dcc0b15e2de4d03a73d0eb0ae8ceb26fe9
Trist4n <tristan@defachel.fr>
2026-09-13 12:45:55 +0200

    refactor(hypr): import in alphabetical order

--- config/hypr/hyprland.lua
+++ config/hypr/hyprland.lua
@@ -1,11 +1,11 @@
 -- Refer to the wiki for more information.
 -- https://wiki.hypr.land/Configuring/Start/
 
-require("config/keybinds")
-require("config/window-and-workspace")
 require("config/autostart")
-require("config/monitors")
 require("config/environment-variables")
-require("config/look-and-feels")
 require("config/inputs")
+require("config/keybinds")
+require("config/look-and-feels")
 require("config/misc")
+require("config/monitors")
+require("config/window-and-workspace")
